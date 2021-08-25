226fd736201065903ea1e1fecae5679cddd83b2f
mikkygee <kevwedeke@gmail.com>
2021-08-25 15:20:17 -0700

    simple shell

--- main.c
+++ main.c
@@ -1,37 +1,36 @@
 #include "shell_inc.h"
 
 /**
- *  * func_loop - Split a line into tokens.
- *   *
- *    * Return: Null-terminated array of tokens.
- *     */
+ * func_loop - Split a line into tokens.
+ *
+ * Return: Null-terminated array of tokens.
+ */
 void func_loop(void)
 {
-	    char *line;
-	        char **args;
-		    int status;
+  char *line;
+  char **args;
+  int status;
 
-		        do {
-				        printf("$ ");
-					        line = func_read_line();
-						        args = func_split_line(line);
-							        status = func_execute(args);
+  do {
+    printf("$ ");
+    line = func_read_line();
+    args = func_split_line(line);
+    status = func_execute(args);
 
-								        free(line);
-									        free(args);
-										    } while (status);
+    free(line);
+    free(args);
+  } while (status);
 }
 
 /**
- *  * main - Entry point.
- *   *
- *    * Return: status code
- *     */
+ * main - Entry point.
+ *
+ * Return: status code
+ */
 int main(void)
 {
-	    func_loop();
-
-	        return (EXIT_SUCCESS);
+  func_loop();
+  
+  return (EXIT_SUCCESS);
 }
-
 
